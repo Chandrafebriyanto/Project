0046358e3c7763731d13dc515f7f0da552689acd
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-04-08 21:25:51 +0700

    project done

--- mini-project-js/calculator/script.js
+++ mini-project-js/calculator/script.js
@@ -5,9 +5,14 @@ function appendToDisplay(input) {
 }
 
 function clearDisplay() {
-    
+    display.value = "";
 }
 
 function calculate() {
-
+    try{
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error!";
+    }
 }
