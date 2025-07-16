fd29a19be4260d6c71566321a8a59a5445ca5b7f
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-07-16 14:16:21 +0700

    commit

--- homework-web/js/script.js
+++ homework-web/js/script.js
@@ -1,10 +1,47 @@
-// note: click task
-const detailTask = () =>{
-    document.getElementById('detail-task').style.display = 'block';
-}
+// note: variabel
+const nameTask = document.getElementById("nameTask");
+const dateline = document.getElementById("dateline");
+const describe = document.getElementById("describe");
+const link = document.getElementById("link");
+
+
+// note:detail task
+const detailTask = (element) => {
+  document.getElementById("detail-task").style.display = "block";
+  if (element.id === "task1") {
+    let tittle = "Membuat Website dengan NodeJS dan Bootstrap";
+    let date = "2025-10-24: 23.59";
+    let linkTask = "https://github.com";
+    let describe_task =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore,saepe? Rerum nisi dignissimos harum, quae odit porro.";
+
+    nameTask.textContent = tittle;
+    dateline.textContent = date;
+    describe.textContent = describe_task;
+    link.href = linkTask;
+  }
+  if (element.id === "task2") {
+    let tittle = "Membuat Website dengan Laravel";
+    let date = "2025-10-24: 23.59";
+    let linkTask = "https://youtube.com";
+    let describe_task =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore,saepe? Rerum nisi dignissimos harum, quae odit porro.";
+
+    nameTask.textContent = tittle;
+    dateline.textContent = date;
+    describe.textContent = describe_task;
+    link.href = linkTask;
+  }
+};
 
 // note: button back
-document.querySelector('.back').addEventListener('click', () => {
-    document.getElementById('detail-task').style.display = 'none';
-})
+document.querySelector(".back").addEventListener("click", () => {
+  document.getElementById("detail-task").style.display = "none";
+});
 
+// note: click detail task
+document.querySelectorAll(".container-task").forEach((task) => {
+  task.addEventListener("click", () => {
+    detailTask(task);
+  });
+});
---
+++ homework-web/test.py
@@ -0,0 +1,7 @@
+age = 24
+has_license = True
+
+result = age >= 17 and has_license
+
+# Don't change the line below
+print("Eligible to drive:", result)
