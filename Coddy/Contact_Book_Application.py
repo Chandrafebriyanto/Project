fb02b19f6a9538c3e1ad5e80ec1fe80a8e8a0020
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-07-28 18:13:59 +0700

    commit

--- Coddy/Contact_Book_Application.py
+++ Coddy/Contact_Book_Application.py
@@ -1,57 +1,51 @@
 contact_book = {"Bob": {"phone": "234-567-8901", "email": "bob@example.com", "address": "456 Oak Ave"}}
 
 def display_menu():
-    print("""Contact Book Menu:
-1. Add Contact
-2. View Contact
-3. Edit Contact
-4. Delete Contact
-5. List All Contacts
-6. Exit""")
-    
-    
-    choice = int(input())
-    if choice == 1:
-        add_contact(contact_book)
-    elif choice == 2:
-        view_contact(contact_book)
-    elif choice == 3:
-        edit_contact(contact_book)
-    elif choice == 4:
-        delete_contact(contact_book)
-    elif choice == 5:
-        list_contacts(contact_book)
-    elif choice == 6:
-        print("Exiting the Contact Book.")
-    else:
-        print("Invalid choice. Please try again.")
+    while True:
+        print("Contact Book Menu:")
+        print("1. Add Contact")
+        print("2. View Contact")
+        print("3. Edit Contact")
+        print("4. Delete Contact")
+        print("5. List All Contacts")
+        print("6. Exit")
+
+        choice = int(input())
+        if choice == 1:
+            add_contact(contact_book)
+        elif choice == 2:
+            view_contact(contact_book)
+        elif choice == 3:
+            edit_contact(contact_book)
+        elif choice == 4:
+            delete_contact(contact_book)
+        elif choice == 5:
+            list_contacts(contact_book)
+        elif choice == 6:
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 def add_contact(contact_book):
     name = input()
     phone = input()
     email = input()
-    address = input()
-    
+    address = input()  
     if name in contact_book:
         print(f"Contact already exists!")
-        display_menu()
     else:
         contact_book[name] = {"phone": phone, "email": email, "address": address}
         print(f"Contact added successfully!")
-        display_menu()
-
+        
 def view_contact(contact_book):
-    name = input()
+    name = input("")
     if name in contact_book:
         print(f"Name: {name}")
-        for key, Value in contact_book[name].items():
-            key = key.capitalize()
-            print(f"{key}: {Value}")
-        display_menu()
+        for key, value in contact_book[name].items():
+            print(f"{key.capitalize()}: {value}")
     else: 
         print("Contact not found!")
-        display_menu()
-        
+
 def edit_contact(contact_book):
     name = input()
     if name in contact_book:
@@ -59,34 +53,26 @@ def edit_contact(contact_book):
             new_value = input()
             if new_value:
                 contact_book[name][key] = new_value
-        print("Contact updated successfully!")
-        display_menu()
+        print("Contact updated successfully!")        
     else:
         print("Contact not found!")
-        display_menu()
-
+        
 def delete_contact(contact_book):
     name = input()
     if name in contact_book:
         del contact_book[name]
-        print(f"Contact deleted successfully!")
-        display_menu()
+        print(f"Contact deleted successfully!")        
     else:
         print("Contact not found!")
-        display_menu()
-    
+
 def list_contacts(contact_book):
     if contact_book:
         for name, details in contact_book.items():
             print(f"Name: {name}")
             for key, value in details.items():
                 print(f"{key.capitalize()}: {value}")
-            print("")
-            display_menu()
+            print("")           
     else:
         print("No contacts available.")
-        display_menu()
-
-while True:
-    display_menu()
-    
+        
+display_menu()
